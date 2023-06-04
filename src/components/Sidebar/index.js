23832bef35697d02862550bc097676e5f524289e
Christian Brown <robrowno@icloud.com>
2023-06-04 10:59:53 +0100

    add icons and style accordingly to the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-c.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = () => (
@@ -11,6 +13,17 @@ const Sidebar = () => (
             <img className="sub-logo" src={LogoSubtitle} alt="logo 2" />
 
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-page" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-page" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
     </div>
 )
 
